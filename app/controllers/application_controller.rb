0fcd93f781f40f84edb1ca755cbf315e0de919ff
rjohnson2011 <ryan.johnson@oddball.io>
2025-08-13 00:25:32 -0600

    Add dummy verify_authenticity_token to fix cron job
    - Render's cron job is using cached Docker image with old auth_controller
    - The old auth_controller tries to skip_before_action :verify_authenticity_token
    - This fails because API controllers don't have this callback
    - Adding empty method prevents the ArgumentError
    - Temporary workaround until Render's cache eventually clears

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  # Add a dummy verify_authenticity_token method to fix cron job error
+  # The cached Docker image has an old auth_controller.rb that tries to skip this
+  # This is a temporary workaround until Render's cache clears
+  def verify_authenticity_token
+    # Do nothing - this is just to prevent the ArgumentError
+    # in the cached auth_controller.rb file
+  end
 end
